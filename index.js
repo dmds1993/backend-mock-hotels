4ed7d11acd8a77cdd7c449d2c1795d9881b382cd
George Marcel Souza Leão <midiasp@gmail.com>
2016-09-20 17:12:45 -0300

    Add CORS plugin

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express     = require('express');
 var app         = express();
+var cors        = require('cors')
 var http        = require('http')
 var server      = http.createServer(app);
 var io          = require('socket.io')(server);
@@ -10,11 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 app.set('port', (process.env.PORT || 5050));
 
