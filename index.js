e35f82420fdd4cc63f080a3e7b56fa2ff948d08e
Juan Esteban Arango <juanes.ao@gmail.com>
2016-09-19 22:26:34 -0500

    Commented the headers validation

--- index.js
+++ index.js
@@ -40,19 +40,19 @@ app.use('/dev/login', login);
 // Middleware Validate the Header
 app.use(function (req, res, next) {
   if(req.method != 'OPTIONS') {
-    if(!req.headers['gtw-sec-user-token']) {
-      return res.status(400).send({ 
-        code: 400, 
-        message: 'Missing header: Gtw-Sec-User-Token' 
-      });
-    }
+    // if(!req.headers['gtw-sec-user-token']) {
+    //   return res.status(400).send({ 
+    //     code: 400, 
+    //     message: 'Missing header: Gtw-Sec-User-Token' 
+    //   });
+    // }
 
-    if (!req.headers['gtw-transaction-id']) {
-      return res.status(400).send({ 
-        code: 400, 
-        message: 'Missing header: Gtw-Transaction-Id' 
-      });
-    }
+    // if (!req.headers['gtw-transaction-id']) {
+    //   return res.status(400).send({ 
+    //     code: 400, 
+    //     message: 'Missing header: Gtw-Transaction-Id' 
+    //   });
+    // }
 
     // if (!req.headers['content-type'] || (req.headers['content-type'] !== 'application/json') ) {
     //   return res.status(400).send({ 
