de1c78d2eb5e94f1c8be7e0cdef8030641dd66df
George Marcel Souza Leão <midiasp@gmail.com>
2016-10-06 15:06:26 -0300

    Update Abroadcourses

--- routes/abroadcourses.js
+++ routes/abroadcourses.js
@@ -128,53 +128,209 @@ var dates = {
 };
 
 var abroadcourses = {
-  "AbroadCourses": [
-    {
-      "abroadCourseToken": "GFDG897GJSDJKFOIU98SDIZXC98BX98SSSDA==.LKMXVOIUVXCKJLVLKJSZDLKJSD98DFS98XC98BX==",
-      "course": {
-        "@TODO": "ADD SAMPLE COURSE"
+  "AbroadCourses": [{
+    "abroadCourseToken": "GFDG897GJSDJKFOIU98SDIZXC98BX98SSSDA==.LKMXVOIUVXCKJLVLKJSZDLKJSD98DFS98XC98BX==",
+    "course": {
+      "name": "Standard English",
+      "language": {
+        "id": "2",
+        "name": "Inglês"
       },
-      "accommodation": {
-        "@TODO": "ADD SAMPLE ACCOMMODATION"
+      "category": {
+        "id": "1",
+        "name": "Geral de Idioma"
       },
-      "isCustomizable": true,
+      "institution": "10792060",
+      "durationWeeks": 1,
+      "classesPerWeek": 20,
+      "classPeriod": "manhã",
+      "start": "2016-08-22",
+      "end": "2016-08-26",
+      "minimumProficiency": {
+        "id": 2,
+        "name": "Básico"
+      },
+      "minimumAge": 18,
+      "maximumAge": 99,
+      "description": "No Standard English Course, você concentra na aprendizagem através da comunicação na vida real (fala, escuta, leitura, escrita) dentro e fora da sala de aula. Este é um curso geral de lingua inglesa de 20 aulas por semana, ministrado em turmas pequenas, o que permite que nossos experientes professores fornecerem feedback individual e aconselhamento sobre como melhorar e desenvolver competências linguísticas.",
+      "keyFeatures": [{
+        "features": [{
+          "label": "Duração:",
+          "value": "4 semanas + 1 semana grátis20 aulas por semana",
+          "note": ""
+        }, {
+          "value": "20 aulas por semana"
+        }]
+      }, {
+        "features": [{
+          "label": "Início:",
+          "value": "21/12"
+        }, {
+          "label": "Término:",
+          "value": "01/01"
+        }]
+      }, {
+        "features": [{
+          "label": "Nível Mínimo:",
+          "value": "Intermediário",
+          "note": "O help deverá vir aqui"
+        }]
+      }, {
+        "features": [{
+          "label": "Recesso:",
+          "value": "de 21/12 a 01/01"
+        }]
+      }],
+      "contents": [{
+        "code": "features",
+        "items": [{
+          "name": "Até 10 alunos por sala."
+        }, {
+          "name": "A escola oferece um coordenador que dará todo o suporte necessário durante o curso."
+        }, {
+          "name": "30 aulas de inglês geral por semana, cada uma com 45 minutos de duração"
+        }]
+      }, {
+        "code": "requirements",
+        "items": [{
+          "name": "Cópia do passaporte"
+        }, {
+          "name": "Ficha de matrícula CVC"
+        }]
+      }, {
+        "code": "documents",
+        "description": "Para realizar este curso você precisará ter o passaporte válido por no mínimo 6 meses e providenciar um visto de turismo junto ao consulado no Brasil. Verifique o tempo necessário junto ao consulado. Recomendamos que a matrícula seja feita com a maior antecedência possível."
+      }, {
+        "code": "promotional-info",
+        "description": "15% de desconto para este curso."
+      }],
       "rate": {
+        "courseToken": "ZS4R5DFCFDESRDVDDEWEWFZFZEGSREGE4==",
+        "currency": "BRL",
         "pricePerPerson": {
-          "currency": "BRL",
           "withTax": 1891.07,
           "withoutTax": 1891.07
         },
-        "taxes": [
-          {
+        "taxes": [{
+          "code": "TX_REM",
+          "description": "Taxa de Remessa",
+          "percent": 0.06,
+          "amount": 31.62,
+          "inTotal": true
+        }]
+      }
+    },
+    "accommodation": {
+      "broker": "ITC",
+      "category": {
+        "id": 12,
+        "name": "Residencia Estudantil"
+      },
+      "name": "BSL BROOKLYN CROWN HEIGHTS INTERCAMBIO",
+      "description": "Esta casa é compartilhada apenas com outros estudantes de Brooklyn School of Languages, e tem 5 quartos (incluindo 3 singles, 1 duplo e 1 twin), 2 banheiros e uma cozinha grande. A cozinha tem uma gama completa de aparelhos modernos, incluindo um grande frigorífico e microondas.",
+      "allowExtraDays": true,
+      "earliestCheckIn": "2016-08-19",
+      "earliestCheckOut": "2016-08-28",
+      "latestCheckOut": "2016-08-29",
+      "checkIn": "2016-08-21",
+      "checkOut": "2016-08-27",
+      "zone": {
+        "id": 25651,
+        "address": "40, RECTOR STREET, NEW YORK, NEW YORK, MANHATTAN, ESTADOS UNIDOS DA AMERICA",
+        "latitude": 40.723143,
+        "longitude": -74.00764
+      },
+      "rooms": [{
+        "id": "400",
+        "category": {
+          "id": 1,
+          "name": "SINGLE"
+        },
+        "bathroomCategory": {
+          "id": 1,
+          "name": "COMPARTILHADO"
+        },
+        "mealPlan": {
+          "id": 1,
+          "name": "SEM REFEICOES"
+        },
+        "capacity": 1,
+        "rate": {
+          "accommodationToken": "LKJVASDLJIV98WKNL98LKWAEXZ==",
+          "currency": "BRL",
+          "pricePerPerson": {
+            "withTax": 1891.07,
+            "withoutTax": 1891.07
+          },
+          "taxes": [{
             "code": "TX_REM",
             "description": "Taxa de Remessa",
             "percent": 0.06,
             "amount": 31.62,
             "inTotal": true
-          }
-        ]
-      },
-      "contents": [
-        {
-          "code": "features",
-          "itens": [
-            {
-              "code": "hotel",
-              "name": "Hotel"
-            },
-            {
-              "code": "car",
-              "name": "Aluguel de Carros"
-            },
-            {
-              "code": "assistance",
-              "name": "Assistência Viagem"
-            }
-          ]
+          }]
         }
-      ]
-    }
-  ],
+      }],
+      "contents": [{
+        "code": "included-services",
+        "items": [{
+          "code": "lazer",
+          "name": "Lazer",
+          "items": [{
+            "name": "Sala de TV"
+          }, {
+            "name": "Sala de Jogos"
+          }]
+        }, {
+          "code": "gastronomy",
+          "name": "Gastronomia",
+          "items": [{
+            "name": "Bar"
+          }, {
+            "name": "Café da Manhã"
+          }]
+        }, {
+          "code": "services",
+          "name": "Serviços",
+          "items": [{
+            "name": "Wifi"
+          }, {
+            "name": "Recepção 24h"
+          }, {
+            "name": "Estacionamento Gratuito"
+          }]
+        }]
+      }]
+    },
+    "isCustomizable": true,
+    "rate": {
+      "currency": "BRL",
+      "pricePerPerson": {
+        "withTax": 1891.07,
+        "withoutTax": 1891.07
+      },
+      "taxes": [{
+        "code": "TX_REM",
+        "description": "Taxa de Remessa",
+        "percent": 0.06,
+        "amount": 31.62,
+        "inTotal": true
+      }]
+    },
+    "contents": [{
+      "code": "features",
+      "itens": [{
+        "code": "hotel",
+        "name": "Hotel"
+      }, {
+        "code": "car",
+        "name": "Aluguel de Carros"
+      }, {
+        "code": "assistance",
+        "name": "Assistência Viagem"
+      }]
+    }]
+  }],
   "meta": {
     "links": {
       "coverImage": {
@@ -192,39 +348,47 @@ var abroadcourses = {
       "maxWithTax": 1647.75,
       "maxWithoutTax": 1647.75
     },
-    "languages": [
-      {
-        "id": "2",
-        "name": "Inglês",
-        "amount": 10
+    "languages": [{
+      "id": "2",
+      "name": "Inglês",
+      "amount": 10
+    }],
+    "categories": [{
+      "id": "1",
+      "name": "Geral de Idioma",
+      "amount": 10
+    }],
+    "loadsTimeCourse": [{
+      "id": "3",
+      "description": "20 a 25 aulas por semana",
+      "amount": 10
+    }, {
+      "id": "4",
+      "name": "mais de 26 aulas por semana",
+      "amount": 10
+    }],
+    "institutions": [{
+      "id": "10792060",
+      "name": "LSI NOVA YORK INTERCAMBIO",
+      "description": "É importante questionar o quanto a consulta aos diversos militantes nos obriga à análise do fluxo de informações. As experiências acumuladas demonstram que a determinação clara de objetivos deve passar por modificações independentemente das diretrizes de desenvolvimento para o futuro.",
+      "zone": {
+        "id": 25651,
+        "address": "40, RECTOR STREET, NEW YORK, NEW YORK, MANHATTAN, ESTADOS UNIDOS DA AMERICA",
+        "city": {
+          "name": "NEW YORK"
+        },
+        "state": {
+          "name": "NEW YORK"
+        },
+        "country": {
+          "name": "ESTADOS UNIDOS DA AMERICA"
+        },
+        "latitude": 40.723143,
+        "longitude": -74.00764
       }
-    ],
-    "categories": [
-      {
-        "id": "1",
-        "name": "Geral de Idioma",
-        "amount": 10
-      }
-    ],
-    "loadsTimeCourse": [
-      {
-        "id": "3",
-        "description": "20 a 25 aulas por semana",
-        "amount": 10
-      },
-      {
-        "id": "4",
-        "name": "mais de 26 aulas por semana",
-        "amount": 10
-      }
-    ],
-    "institutions": [
-      {
-        "@TODO": "ADD SAMPLE INSTITUTION"
-      }
-    ]
+    }]
   }
-};
+};  
 
 var rateTokenCourse = {
   "AbroadCourse": {
