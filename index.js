3303818a09a4949fdd659e129e8f34b3faa3e37f
VitorGiammella <vitor.giammella@soaexpert.com>
2017-03-09 11:30:20 -0300

    Created mock sessionvalidate

--- index.js
+++ index.js
@@ -87,6 +87,9 @@ app.use('/dev/flights', flights);
 var cars = require('./routes/cars');
 app.use('/dev/cars', cars);
 
+var sessionvalidate = require('./routes/sessionvalidate'); 
+app.use('/dev/sessionvalidate', sessionvalidate);
+
 var payments = require('./routes/payments');
 app.use('/dev/payments', payments);
 
