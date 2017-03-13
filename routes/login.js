e6db33fb30d2f18621d31340c03c51670a9c33a2
inagano <ianngn@gmail.com>
2017-03-13 11:18:41 -0300

    adicionadas respostas PASSWORD_EXPIRED e USER_BLOCKED

--- routes/login.js
+++ routes/login.js
@@ -3,9 +3,26 @@ var router = express.Router();
 
 router.post('/', function(req, res, next) {
 	if(req.body.username.length && req.body.password.length) {
-  	res.json({ secUserToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVkZW50aWFsIjp7InBlcnNvbklkIjo3NjI3ODY1LCJ1c2VySWQiOjk2NzExLCJuYW1lIjoiTUFSQ1VTIFZJTklDSVVTIENPU1RBIEUgUEFVTEEiLCJjcGYiOiI0MTA1Mjk2NDg1MCIsImJyYW5jaElkIjoxMDAwLCJhZ2VudFNpZ24iOiJXRUIiLCJ1c2VyIjoiTVRaQ1BENjAwIn0sInN5c3RlbXMiOlt7InN5c3RlbUNvZGUiOjEsImNsaWVudElEIjo5ODUxOTE0NjcxNzIxODE2LCJleHBpcmVzT24iOiIyMDE2LTA5LTExVDE0OjU5OjQ1LjAwMCswMDowMCIsImFjdGl2ZSI6IlMgIn0seyJzeXN0ZW1Db2RlIjozLCJjbGllbnRJRCI6OTg1MTkxNDY3MTcyMTgxNiwiZXhwaXJlc09uIjoiMjAxNi0wOS0xMVQxNDoxNDo0NS4wMDArMDA6MDAiLCJhY3RpdmUiOiJTICJ9XSwiaWF0IjoxNDczNjEzMTg4fQ.u6e0Pcix_QzSohgh4vcZULOopjN51w-328UGdJ9BHyM',profile:{name:"MARCUS VINICIUS COSTA E PAULA",userType:"VENDOR"} });
+		if(req.body.password === 'expired') {
+			res.json({
+				reason: 'PASSWORD_EXPIRED',
+				code: 'PXP'
+			});
+		} else {
+			if(req.body.password === 'blocked') {
+				res.json({
+					reason: 'USER_BLOCKED',
+					code: 'UBL'
+				});
+			} else {
+  			res.json({ secUserToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVkZW50aWFsIjp7InBlcnNvbklkIjo3NjI3ODY1LCJ1c2VySWQiOjk2NzExLCJuYW1lIjoiTUFSQ1VTIFZJTklDSVVTIENPU1RBIEUgUEFVTEEiLCJjcGYiOiI0MTA1Mjk2NDg1MCIsImJyYW5jaElkIjoxMDAwLCJhZ2VudFNpZ24iOiJXRUIiLCJ1c2VyIjoiTVRaQ1BENjAwIn0sInN5c3RlbXMiOlt7InN5c3RlbUNvZGUiOjEsImNsaWVudElEIjo5ODUxOTE0NjcxNzIxODE2LCJleHBpcmVzT24iOiIyMDE2LTA5LTExVDE0OjU5OjQ1LjAwMCswMDowMCIsImFjdGl2ZSI6IlMgIn0seyJzeXN0ZW1Db2RlIjozLCJjbGllbnRJRCI6OTg1MTkxNDY3MTcyMTgxNiwiZXhwaXJlc09uIjoiMjAxNi0wOS0xMVQxNDoxNDo0NS4wMDArMDA6MDAiLCJhY3RpdmUiOiJTICJ9XSwiaWF0IjoxNDczNjEzMTg4fQ.u6e0Pcix_QzSohgh4vcZULOopjN51w-328UGdJ9BHyM',profile:{name:"MARCUS VINICIUS COSTA E PAULA",userType:"VENDOR"} });
+			}
+		}
 	} else {
-  	res.json({ reason: 'INVALID_USERNAME-PASSWORD' });
+  	res.json({
+			reason: 'INVALID_USERNAME-PASSWORD',
+			code: 'IUP'
+		});
 	}
 });
 
